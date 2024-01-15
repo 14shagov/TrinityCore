844d7d52c4c72d29ea142b6c7faf1df997f7b9f5
bibi <125542784+14shagov@users.noreply.github.com>
2024-01-15 16:46:56 +0300

    Create zulgurub.cpp

---
+++ src/server/scripts/EasternKingdoms/ZulGurub/zulgurub.cpp
@@ -0,0 +1,64 @@
+/*
+ * This file is part of the TrinityCore Project. See AUTHORS file for Copyright information
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "zulgurub.h"
+#include "GameObject.h"
+#include "GameObjectAI.h"
+#include "InstanceScript.h"
+#include "Map.h"
+#include "ScriptMgr.h"
+
+ /*######
+ ## go_brazier_of_madness
+ ######*/
+
+static ZGCreatureIds const madnessCreatures[] =
+{
+   NPC_HAZZARAH, NPC_WUSHOOLAY, NPC_RENATAKI, NPC_GRILEK
+};
+
+Position const MadnessSpawnPos = { -11901.229f, -1906.366f, 65.358f, 0.942f };
+
+class go_brazier_of_madness : public GameObjectScript
+{
+public:
+    go_brazier_of_madness() : GameObjectScript("go_brazier_of_madness") { }
+
+    struct go_brazier_of_madnessAI : public GameObjectAI 
+    {
+        go_brazier_of_madnessAI(GameObject* go) : GameObjectAI(go) { }
+
+        bool OnGossipHello(Player* player) override
+        {
+            me->UseDoorOrButton();
+            uint8 Random = urand(0, 3);
+            player->SummonCreature(madnessCreatures[Random], MadnessSpawnPos, TEMPSUMMON_DEAD_DESPAWN, 2h);
+            return false;
+        }
+    };
+
+    GameObjectAI* GetAI(GameObject* go) const override
+    {
+        return new go_brazier_of_madnessAI(go);
+    }
+};
+
+
+void AddSC_zulgurub()
+{
+    new go_brazier_of_madness();
+}
